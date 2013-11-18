6758a6e21123c5b7c614afe5996f48fe98284e64
Timo Gmell <t.gmell@sofort.com>
2013-11-18 23:37:43 +0100

    New version of test.py

--- test.py
+++ test.py
@@ -1,43 +1,95 @@
 #!/usr/bin/python
 
 import os.path
+import datetime
 
-def gitModified(gitProject, gitPath):
-	homeDir = getHomeDir()
-	cacheResult = readLines(homeDir + '/.gitcache/' + gitProject)
-	print cacheResult
-	gitIndex = gitIndexModified(gitPath + '/.git/index')
-	print gitIndex
-	return True
+class GitStatus:
 
-def readCacheFile( path ):
-	result = []
-	
+	__project = ""
+	__path = ""
+	__cacheFile = ""
 
-def gitIndexModified( myFile ):
-	mtime = os.path.getmtime(myFile)
-	return mtime
+	__cacheExpires = 0
+	__cacheIndex = ""
+	__cacheHead = ""
+	__cacheFetchHead = ""
+	__cacheStatus = ""
+
+	__newExpires = 0
+	__newIndex = ""
+	__newHead = ""
+	__newFetchHead = ""
+	__newStatus = ""
 
 
-def readLines( myFile ):
-	lines = []
+	def __init__(self, project, path):
+		self.__project = project
+		self.__path = path
+		self.__cacheFile = os.path.expanduser('~') + '/.gitcache/' + self.__project
 
-	if os.path.isfile(myFile) :
+		cache = self.__readCacheFile()
+		cache = map(lambda c: c.strip(), cache)
+
+		self.__cacheExpires = os.path.getmtime(self.__cacheFile)
+		self.__cacheIndex = cache[0]
+		# self.__cacheHead = cache[1]
+		# self.__cacheFetchHead = cache[2]
+		self.__cacheStatus = cache[3] 
+
+		self.__newExpires = datetime.datetime.now().time()
+		self.__newIndex = str(os.path.getmtime(self.__path + '/.git/index'))
+
+
+	def __repr__(self):
+		return 'GitStatus: project={project} expires={expires} cache={cache}'.format(
+			project=self.__project, expires=self.__cacheExpires,
+			cache=[self.__cacheIndex, self.__cacheHead, self.__cacheFetchHead, self.__cacheStatus])
+
+
+	def writeCacheFile(self):
 		try:
-			with open(myFile) as f:
-				lines = f.readlines()
+			with open(self.__cacheFile + '_new', 'w') as f:
+				f.write(self.__newIndex + '\n')
+				f.write(self.__newHead + '\n')
+				f.write(self.__newFetchHead + '\n')
+				f.write(self.__newStatus)
 			f.close()
 		except IOError:
-			print 'Failed to open file for reading: {}'.format(myFile)
-	else:
-		print 'File does not exist: {}'.format(myFile)
-
-	return lines 
+			print 'Failed to write file: {}'.format(self.__cacheFile)
 
 
-def getHomeDir():
-	return os.path.expanduser('~')
+	def isCacheDirty(self):
+		expired = self.isCacheExpired()
+		indexDirty = self.__isIndexDirty()
+		return True if expired or indexDirty else False
 
 
-myFile = '/cygdrive/d/git-hub/dotfiles'
-print gitModified('dotfiles', myFile)
+	def isCacheExpired(self):
+		return True if self.__newExpires != self.__cacheExpires else False
+
+
+	def __isIndexDirty(self):
+		return True if self.__newIndex != self.__cacheIndex else False
+
+
+	def __readCacheFile(self):
+		lines = []
+
+		if os.path.isfile(self.__cacheFile) :
+			try:
+				with open(self.__cacheFile, 'r') as f:
+					lines = f.readlines()
+				f.close()
+			except IOError:
+				print 'Failed to open file for reading: {}'.format(self.__cacheFile)
+		else:
+			print 'File does not exist: {}'.format(self.__cacheFile)
+
+		return lines 
+
+
+myFile = '/cygdrive/s/git-hub/inxz/dotfiles'
+GitStatus = GitStatus('dotfiles', myFile)
+print GitStatus
+print GitStatus.isCacheDirty()
+GitStatus.writeCacheFile()
